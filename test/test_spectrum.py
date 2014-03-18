948c8ad4aae42440eed09ea2eea37758ec26544a
ariamania <maniadaki@materials.uoc.gr>
2014-03-19 00:41:45 +0100

    finalize the test_copy in test_spectrum.py

--- test/test_spectrum.py
+++ test/test_spectrum.py
@@ -2,32 +2,29 @@ import spectraplotpy as spp
 import numpy as np
 
 def test_construction():
-    ds = [1,2,3,4]   
-    s = spp.Spectrum(ds)
-    assert s
+    ds = spp.Dataset()
+    assert spp.Spectrum(ds)
 
 def test_copy():
-    ds = [1,2,3,4]
+    ds = spp.Dataset()
     s = spp.Spectrum(ds)
+#    s.dataset.x = [1,2,3,4]
     s1 = s.copy()
-    
-    snp = np.array(ds)
-    snp1 = snp.copy()
-    
-    assert s1 == snp1 
-#    assert s1 == s
+    assert s.dataset.x == s1.dataset.x
+    assert s.dataset.y == s1.dataset.y
+#    assert s == s1
     
 #def test_add():
-#    ds = [1,2,3,4]
-#    s = spp.Spectrum(ds) 
+#    ds = spp.Dataset()
+#    s = spp.Spectrum(ds)
+#    s.dataset.y = np.array([1,2,3,4])
 #    s1 = s.copy()
-#    s2 = s.add(s1)
 #    
-#    snp = np.array(ds)
+#    snp = np.array([1,2,3,4])
 #    snp1 = snp.copy()
-#    snp2 = snp + snp1
+##    snp2 = snp + snp1
 #    
-#    assert s == snp
-#    assert s1 == snp1
-#    assert s2 == snp2
+#    assert all(s.dataset.y == snp)
+#    assert all(s1.dataset.y == snp1)
+##    assert all(s2.dataset.x == snp2)
     
