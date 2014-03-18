7247c5fa4d3ab85e6d5006665a7b4710bb10eb12
mboufatah <boufatah@gmail.com>
2014-03-18 17:17:52 +0100

    Add function write

--- spectraplotpy/exporter.py
+++ spectraplotpy/exporter.py
@@ -3,10 +3,25 @@
 class Exporter(object):
     """ Class Exporter """
 
-    def __init__(self,dataset):
-         """ Initialise the dataset"""
-         pass
+    def __init__(self , dataset):
+        """ Initialise the dataset"""
+        pass
 
     def __str__(self):
+        """ """
+        pass
+    
+    def write(self , filename):
+        """"""
+        self.filename = filename
+        self.write_metadata()
+        self.write_data()
+
+    def write_metadata(self):
+        """"""
+       self.fichier = open(self.filename , 'w')
+       self.fichier.write('Test Writing\n')
+        
+    def write_data(self):
         """ """
         pass
