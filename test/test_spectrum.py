dd25eeb2bd8b8f312d9f6fedd0cbf243d8c2a509
ariamania <maniadaki@materials.uoc.gr>
2014-03-18 18:23:12 +0100

    implemented test_copy in the test_spectrum

--- test/test_spectrum.py
+++ test/test_spectrum.py
@@ -4,4 +4,10 @@ def test_construction():
     ds = [1,2,3,4]   
     s = spp.Spectrum(ds)
     assert s
-      
+
+def test_copy():
+    ds = [1,2,3,4]
+    s = spp.Spectrum(ds)
+    s1 = s.copy()
+    assert s1
+    
