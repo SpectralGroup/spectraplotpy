7b1b0033d52063c056d41623248364f88bec1bb8
mboufatah <boufatah@gmail.com>
2014-03-18 19:29:53 +0100

    Exporter passing the linter.

--- spectraplotpy/exporter.py
+++ spectraplotpy/exporter.py
@@ -3,32 +3,31 @@
 class Exporter(object):
     """ Class Exporter """
 
-    def __init__(self , dataset):
-        """ Initialise the dataset"""
-        pass
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.filename = ""
 
     def __str__(self):
-        """ """
         pass
     
     def write(self , filename):
-        """"""
+        """This function writest the content of the dataset to a file"""
         self.filename = filename
         self.write_metadata()
         self.write_data()
 
     def write_metadata(self):
-        """"""
-        self.fichier = open(self.filename , 'w')
-        self.fichier.write('Test Writing\n')
+        """This writes the metadata to a given file"""
+        foutput = open(self.filename , 'w')
+        foutput.write('Test Writing\n')
         
     def write_data(self):
-        """ """
+        """ This writes the data to a given file """
         pass
     
-    def plot_data(self,dataset):
+    def plot_data(self):
         """ Plotting Data with Matplotlib"""
-        import matplotlib as plt
-        plt.plot(dataset)
+        import matplotlib.pyplot as plt
+        plt.plot(self.dataset.x, self.dataset.y)
         plt.show()
         
