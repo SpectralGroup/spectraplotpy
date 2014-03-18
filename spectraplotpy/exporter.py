92290a093cafef249a5b96c0870d3ed604db11c4
mboufatah <boufatah@gmail.com>
2014-03-18 16:49:24 +0100

    First Definitions of exporter

---
+++ spectraplotpy/exporter.py
@@ -0,0 +1,12 @@
+""" Exporter Program"""
+
+class Exporter(object):
+    """ Class Exporter """
+
+    def __init__(self,dataset):
+         """ Initialise the dataset"""
+         pass
+
+    def __str__(self):
+        """ """
+        pass
