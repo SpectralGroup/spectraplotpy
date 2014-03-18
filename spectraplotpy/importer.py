72569c2fac3d593aa1c299ea7ff5e0dee57ce076
Lidia Bressan <lidia.bressan@gmail.com>
2014-03-18 18:11:23 +0100

    Stylish modifications for Oscar....

--- spectraplotpy/importer.py
+++ spectraplotpy/importer.py
@@ -1,37 +1,43 @@
 #from dataset import dataset
+"""The importer class allows you to read data from file."""
 
 
 class Importer(object):
     """The importer class allows you to read data from file."""
 
     def __init__(self, filename):
+        """Constructor"""
         self.dataset = self.load(filename)
-        
+
     def load(self, filename):
-        raise Exception('loadd')   
-        
-   
-      
-            
-            
-class AvivImporter(Importer) :
-    
-     def load(self, filename):
-         with open(filename) as f:
-             whole_text=f.read()
-             #print whole_text.index('_data_')
-             data=whole_text[whole_text.index('\n_data_')+7: whole_text.index('\n_data_end_')]
-             metadata = 'filename = ' + filename + '\n'
-             metadata = metadata + whole_text[0:whole_text.index('\n_data_')] + whole_text[whole_text.index('_data_end_')+10:]
-             #print metadata
-             
-             
-             
-         
-   
+        """Load method"""
+        raise Exception('Cannot load ' + filename + 'in baseclass.')
 
 
-   
+class AvivImporter(Importer):
+    """ Importer of Aviv files
+    """
+
+    def load(self, filename):
+        """Load method"""
+        with open(filename) as inputfile:
+            whole_text = inputfile.read()
+            #print whole_text.index('_data_')
+            start = whole_text.index('\n_data_')+7
+            end = whole_text.index('\n_data_end_')
+            data = whole_text[start+7:end]
+            print len(data)
+            metadata = 'filename = ' + filename + '\n'
+            metadata = metadata + whole_text[0:start] + whole_text[end+10:]
+            #print metadata
+
+
+
+
+
+
+
+
 
 
 
