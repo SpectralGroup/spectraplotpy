ca360a0daeb0b04487d6915d2c2b7d989807ecb6
Lidia Bressan <lidia.bressan@gmail.com>
2014-03-18 17:35:54 +0100

    imported the importer in the spectraplotpy lib ( __init__.py) defined importer class and aviv importer subclass. defined test for aviv importer and importer.

--- spectraplotpy/importer.py
+++ spectraplotpy/importer.py
@@ -3,16 +3,35 @@
 
 class Importer(object):
     """The importer class allows you to read data from file."""
-    #self.dataset = Dataset()
-    
+
     def __init__(self, filename):
-        self.dataset = None
-        with open(filename) as f:
-            whole_text=f.read()
-            print whole_text
-#        pass
-#        return dataset
+        self.dataset = self.load(filename)
+        
+    def load(self, filename):
+        raise Exception('loadd')   
+        
+   
+      
+            
+            
+class AvivImporter(Importer) :
     
+     def load(self, filename):
+         with open(filename) as f:
+             whole_text=f.read()
+             #print whole_text.index('_data_')
+             data=whole_text[whole_text.index('\n_data_')+7: whole_text.index('\n_data_end_')]
+             metadata = 'filename = ' + filename + '\n'
+             metadata = metadata + whole_text[0:whole_text.index('\n_data_')] + whole_text[whole_text.index('_data_end_')+10:]
+             #print metadata
+             
+             
+             
+         
+   
+
+
+   
 
 
 
--- test/test_importer.py
+++ test/test_importer.py
@@ -1,12 +1,22 @@
 import spectraplotpy as spp
+import pytest
 
 
-def test_importer() :
-#    assert spp.Importer.load(nomefile)
-    assert spp.Importer('Data/01-CD-Aviv62DS/CSA/CSA.CD')
-    
+def test_Aviv_importer() :
+    assert spp.AvivImporter('Data/01-CD-Aviv62DS/CSA/CSA.CD')
+    assert spp.AvivImporter('Data/01-CD-Aviv62DS/CSA/blank.CD')
+    assert spp.AvivImporter('Data/01-CD-Aviv62DS/PEP-average/4RNSX.001')
+
+    #print spp.Importer('Data/01-CD-Aviv62DS/PEP-average/4RNSX.001')
+  
+
+def test_baseclass_constructor():
+    with pytest.raises(Exception):
+        spp.Importer('Data/01-CD-Aviv62DS/PEP-average/4RNSX.001')
+
+
 if __name__ == "__main__":
-    test_importer()
+    test_Aviv_importer()
     
     
     
