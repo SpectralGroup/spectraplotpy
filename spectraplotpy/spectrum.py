f001fa8333a11955f96c0bdf4f219c4a20ccbe7f
ariamania <maniadaki@materials.uoc.gr>
2014-03-18 15:59:41 +0100

    starting spectrum.py

---
+++ spectraplotpy/spectrum.py
@@ -0,0 +1,16 @@
+""" spectrum.py """
+class Spectrum(object):
+    """ An object class defining a spectrum """
+    def __init__(self, dataset):
+        """ """
+        pass 
+    def __add__(self, dataset):
+        """ adds two spectra, returns third spectrum """
+        pass 
+    def add(self, dataset):
+        """ adds two spectra in place"""
+        pass
+    
+    def copy(self, dataset):
+        """ creates a copy of a spectrum"""
+        pass
