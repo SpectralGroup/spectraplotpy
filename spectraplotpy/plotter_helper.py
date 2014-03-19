3156e743c6257e22d0eeaddbb953c5051dff5b73
Lidia Bressan <lidia.bressan@gmail.com>
2014-03-19 18:19:14 +0100

    Added comments to plot_spectra.

--- spectraplotpy/plotter_helper.py
+++ spectraplotpy/plotter_helper.py
@@ -5,6 +5,10 @@ import  spectraplotpy as spp
 
 
 def plot_spectra(sp_list, **kwarg):
+    """
+    Function that takes a list of spectra in input and plots them
+    by calling their spectra plot method.
+    """
     for sp in sp_list:
         sp.plot(**kwarg)
 
