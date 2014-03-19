3277d4e6426d64b0c199b0eab8c8ee993c93ba54
ariamania <maniadaki@materials.uoc.gr>
2014-03-19 15:11:05 +0100

    finalized test_add and test_add_value in test_spectrum.py

--- test/test_spectrum.py
+++ test/test_spectrum.py
@@ -1,30 +1,50 @@
 import spectraplotpy as spp
 import numpy as np
 
+def create_fake_dataset():
+    ds = spp.Dataset()
+    ds.x = np.array([1,2,3,4])
+    ds.y = np.array([2,4,6,8])
+    return ds 
+
 def test_construction():
     ds = spp.Dataset()
     assert spp.Spectrum(ds)
 
 def test_copy():
-    ds = spp.Dataset()
-    s = spp.Spectrum(ds)
-#    s.dataset.x = [1,2,3,4]
+    ds = create_fake_dataset()
+    s = spp.Spectrum(ds) 
     s1 = s.copy()
-    assert s.dataset.x == s1.dataset.x
-    assert s.dataset.y == s1.dataset.y
-#    assert s == s1
     
-#def test_add():
-#    ds = spp.Dataset()
-#    s = spp.Spectrum(ds)
-#    s.dataset.y = np.array([1,2,3,4])
-#    s1 = s.copy()
-#    
-#    snp = np.array([1,2,3,4])
-#    snp1 = snp.copy()
-##    snp2 = snp + snp1
-#    
-#    assert all(s.dataset.y == snp)
-#    assert all(s1.dataset.y == snp1)
-##    assert all(s2.dataset.x == snp2)
+    assert all(s.dataset.x == s1.dataset.x)
+    assert all(s.dataset.y == s1.dataset.y)
+    
+    s1.dataset.x[2] = 100
+    assert not all(s.dataset.x == s1.dataset.x)
+    
+    
+def test_add():
+    ds = create_fake_dataset()
+    s = spp.Spectrum(ds)
+    s1 = s.copy()
+    s2 = s + s1
+    s.add(s1)
+    
+    assert all(s.dataset.x == s2.dataset.x)
+    assert all(s.dataset.y == s2.dataset.y)
+
+def test_add_value():
+    s_y_np = np.array([2,4,6,8])
+    s_y_np1 = s_y_np.copy()
+    s_y_np2 = s_y_np + s_y_np1
+    
+    ds = create_fake_dataset()
+    s = spp.Spectrum(ds)
+    s1 = s.copy()
+    
+    assert all(s.dataset.y == s_y_np)
+    
+    s.add(s1)
+    
+    assert all(s.dataset.y == s_y_np2)
     
