0ff5feb82e7d1d26f6b654e163d724200110869c
Lidia Bressan <lidia.bressan@gmail.com>
2014-03-19 15:17:47 +0100

    Improved documentation. General functions moved outside of the class.

--- spectraplotpy/importer.py
+++ spectraplotpy/importer.py
@@ -1,22 +1,86 @@
-"""The importer class allows you to read data from file."""
+"""
+The importer class.
+
+It is used to read data from a file and
+store the data and metadata in a dataset.
+"""
 from  spectraplotpy.dataset import Dataset
 import numpy as np
 import re
 
-class Importer(object):
-    """The importer class allows you to read data from file."""
 
+
+
+def get_txt_data_metadata(text, filename=None):
+    """
+    Function that takes a text (as a single string)
+    and returns the text containing the metadata (as a string)
+    and the data (as a list of strings).
+
+    It identifies every lines starting with "#", "_" or with a letter
+    as metadata, and the rest as data.
+    """
+    text = text.split('\n')
+    data_txt = [line for line in text
+                     if not (line.startswith('_')
+                             or line.startswith('#')
+                             or re.match('[a-zA-Z]', line))]
+    if filename is not None:
+        metadata_txt = 'filename ' + filename + '\n'
+    else:
+        metadata_txt = None
+    return data_txt, metadata_txt
+
+
+
+def parse_metadata(metadata_txt):
+    """
+    Function that returns a dictionary of the metadata
+    from the metadata as a string.
+    """
+    metadata = dict()
+    metadata_txt = metadata_txt.split('\n')
+    for line in metadata_txt:
+        line = line.split()
+        if len(line) > 1:
+            keyword = line[0]
+            value = ' '.join(line[1:])
+            metadata[keyword] = value
+    return metadata
+
+
+
+
+
+class Importer(object):
+    """
+    The importer class allows you to read data from file
+    and store the data into a dataset instance.
+    """
     def __init__(self, filename):
-        """Constructor"""
+        """
+        Constructor: it calls the load function.
+        """
         self.dataset = self.load(filename)
 
 
     def load(self, filename):
         """
-        Load method
+        The load method takes a filename and stores all the information
+        into the dataset.
+
+        It performs the following steps:
+         - read the text file as a string (see take_text);
+         - separate the metadata and the data lines
+            (see get_txt_data_metadata);
+         - store the metadata information in the dataset.metadata dictionary
+            (see parse_metadata);
+         - store the needed information (as for example units, dimensions, ...)
+            of the data in the dataset.metadata instance (see set_info);
+         - store the data into the x, y, errors_x, errors_y of the dataset
+            (see parse_data).
         """
         text = self.take_text(filename)
-
         data_txt, metadata_txt = self.get_txt_data_metadata(text, filename)
         self.dataset.metadata = self.parse_metadata(metadata_txt)
         self.set_info(self.dataset.metadata)
@@ -25,9 +89,8 @@ class Importer(object):
 
     def take_text(self, filename):
         """
-        Read the file and return the text.
+        Read the file and return the text as a string.
         """
-
         whole_text = None
         with open(filename) as inputfile:
             self.dataset = Dataset()
@@ -36,41 +99,35 @@ class Importer(object):
 
 
     def get_txt_data_metadata(self, text, filename=None):
-        """Separate data and metadata information form the text file"""
-        text = text.split('\n')
-        data_txt = [line for line in text
-                         if not (line.startswith('_')
-                            or re.match('[a-z]', line))]
-        metadata_txt = 'filename = ' + filename + '\n'
-        return data_txt, metadata_txt
+        """
+        Separate the data and metadata part from the text file (string).
+
+        It depends on the file format, it should be overriden for each specific
+        importer subclass.
+        """
+        return get_txt_data_metadata(text, filename)
 
 
     def parse_metadata(self, metadata_txt):
         """
-        Transform metadata from metadata_txt to a dictionary
+        Return the metadata information as a dictionary.
         """
-        metadata = dict()
-        metadata_txt = metadata_txt.split('\n')
-        for line in metadata_txt:
-            line = line.split()
-            #print 0, line,
-            if len(line) > 1:
-                keyword = line[0]
-                value = ' '.join(line[1:])
-                metadata[keyword] = value
-        return metadata
+        return  parse_metadata(metadata_txt)
 
 
     def set_info(self, metadata):
         """
-        Defines units and dimensions of the dataset.
+        Defines the particular informations needed for a dataset.
+
+        It can be overridden by subclass methods.
         """
         pass
 
 
     def parse_data(self, data_txt):
         """
-        Parse the text containing the data
+        Parse the text containing the data and store the x, y and errors
+        in the dataset attributes.
         """
         data = np.loadtxt(data_txt)
         self.dataset.x = data[:, 0]
@@ -91,11 +148,15 @@ class Importer(object):
 
 class AvivImporter(Importer):
     """
-    Importer of Aviv files
+    Importer of Aviv files.
     """
     def get_txt_data_metadata(self, text, filename=None):
-        """Separate data and metadata information form the text file"""
-        metadata_txt = 'filename = ' + filename + '\n'
+        """
+        Separate data and metadata information form the text file.
+
+        The data are included between "_data_" and "_data_end_" lines.
+        """
+        metadata_txt = 'filename ' + filename + '\n'
         start = text.index('\n_data_') + 7
         end = text.index('\n_data_end_')
         data_txt = (text[start + 7:end]).split('\r\n')
@@ -105,6 +166,11 @@ class AvivImporter(Importer):
         return data_txt, metadata_txt
 
     def set_info(self, metadata):
+        """
+        Defines the particular informations needed for a dataset.
+
+        It stores dimensions and units in the dataset attributes.
+        """
         self.dataset.dim_x = 'wavelength'
         self.dataset.dim_y = metadata['_y_type_']
         self.dataset.units_x = metadata['x_unit']
