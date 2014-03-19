1ae09ec4540bddbd004b52706bf1d284080eda61
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2014-03-19 14:58:08 +0100

    Adding the base exporter classes.

--- test/test_exporter.py
+++ test/test_exporter.py
@@ -1,59 +1,25 @@
-""" Exporter Program"""
+# -*- coding: utf-8 -*-
 
-class Exporter(object):
-    """ Class Exporter """
+"""
+A couple of tests for the Exporter class and subclasses
+"""
 
-    def __init__(self, dataset):
-        self.dataset = dataset
-        self.filename = ""
+from spectraplotpy import Exporter
+from spectraplotpy import BasePlotExporter
+from spectraplotpy import BaseTextExporter
 
-    def __str__(self):
-        pass
-    
-    def write(self , filename):
-        """This function writest the content of the dataset to a file"""
-        self.filename = filename
-        self.write_metadata()
-        self.write_data()
+def test_construction():
+    """Test the constructor of the class Exporter"""
+    assert Exporter([])
 
-    def write_metadata(self):
-        """This writes the metadata to a given file"""
-        foutput = open(self.filename , 'w')
-        foutput.write('Test Writing\n')
-        
-    def write_data(self):
-        """ This writes the data to a given file """
-        pass
-    
-    def plot_data(self, plotype=None):
-        """ Plotting Data with Matplotlib
-            with arguments
-            lineair
-            semilogy
-            semilogx
-            loglog
-            """
 
-        import matplotlib.pyplot as plt
-        
-        if plotype == 'linear':
-            plt.plot(self.dataset.x, self.dataset.y)
-            plt.title('Linear')
-            
-        if plotype == 'semilogy':
-            plt.semilogy(self.dataset.x, self.dataset.y)
-            plt.title('Semilogy')            
-            plt.grid(True)
-            
-        if plotype == 'semilogx':
-            plt.semilogx(self.dataset.x, self.dataset.y)
-            plt.title('Semilogx')
-            plt.grid(True)
-            
-        if plotype == 'loglog':
-            plt.loglog(self.dataset.x, self.dataset.y)
-            plt.title('loglog')
-            plt.grid(True)
-            
-        plt.show()
-        
+def test_txt_construction():
+    """Test construction of the class BaseTextExporter"""
+    assert BaseTextExporter([])
+
+
+def test_plot_construction():
+    """Test construction of the class BasePlotExporter"""
+    assert BasePlotExporter([])
+
+
