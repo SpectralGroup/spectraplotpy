4f28a7eda43ae5cd172ca23f4683269c6b4bcec9
ariamania <maniadaki@materials.uoc.gr>
2014-03-19 15:12:08 +0100

    added correction to spectrum.py

--- spectraplotpy/spectrum.py
+++ spectraplotpy/spectrum.py
@@ -9,6 +9,9 @@ class Spectrum(object):
         """ """
         self.dataset = dataset
         
+#    def __str__(self):
+#        return 'spectum('self.dataset.metadata.filename))
+
 
 
     def __add__(self, other):
@@ -18,9 +21,13 @@ class Spectrum(object):
         return copied
 
 
+
     def add(self, other):
         """ adds two spectra in place"""
-        self.dataset.y += other.dataset.y 
+        self.dataset.y += other.dataset.y
+        
+
+
     
     def __sub__(self, other):
         """ substracs two spectra, returns third spectrum """
@@ -33,21 +40,28 @@ class Spectrum(object):
         """ substracs two spectra in place"""
         self.dataset.y -= other.dataset.y
             
+            
+            
     def __mul__(self, const):
         """ multiplies a spectrum with number a in place"""
         copied = self.copy()
         copied.mul(const)
         return copied
+
+
         
     def mul(self, const):
         """ multiplies a spectrum with a number """
         self.dataset.y = const * self.dataset.y
+
+
         
     def __div__(self, const):
         """ divides a spectrum with number a in place"""
         copied = self.copy()
         copied.div(const)
         return copied
+
        
             
     def div(self, const):
