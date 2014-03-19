fc06229d8e01217d7a4f6865ce28c82de69fe0da
Lidia Bressan <lidia.bressan@gmail.com>
2014-03-19 18:17:29 +0100

    Added functionality to plot multiple spectra.

--- spectraplotpy/plotter_helper.py
+++ spectraplotpy/plotter_helper.py
@@ -4,8 +4,10 @@ It plots multiple spectra.
 import  spectraplotpy as spp
 
 
-def plot_spectra(sp, **kwarg):
-    sp.plot(**kwarg)
+def plot_spectra(sp_list, **kwarg):
+    for sp in sp_list:
+        sp.plot(**kwarg)
+
 
 
 
