4b2fc5a776371c1a494bccd22af6c1524076c7f3
Oscar David Arbeláez Echeverri <odarbelaeze@gmail.com>
2014-03-21 00:20:51 +0100

    Adding the licence and notice.

--- spectraplotpy/dataset.py
+++ spectraplotpy/dataset.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
+#
+# This file is part of spectraplotpy.
+#
+# spectraplotpy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# spectraplotpy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with spectraplotpy.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 """
 The Dataset class receives and holds inputs from the importer class and holds
 them for further operations by the spectrum and exporter classes.
